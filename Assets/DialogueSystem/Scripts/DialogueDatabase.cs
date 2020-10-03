fba7b9fb95da5fb783d6f70731be045a283db289
BogdanClinciu <48719928+BogdanClinciu@users.noreply.github.com>
2020-10-03 12:51:06 +0300

    Added interactable interface and dialogue system scriptable object

---
+++ Assets/DialogueSystem/Scripts/DialogueDatabase.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "DialogueDatabase", menuName = "LudumDare_2020/DialogueDatabase", order = 0)]
+public class DialogueDatabase : ScriptableObject
+{
+    public string this[int key]
+    {
+        get
+        {
+            if(!intitialized)
+            {
+                Initialize();
+            }
+
+            if(Lines.ContainsKey(key))
+            {
+                return Lines[key];
+            }
+            else
+            {
+                return "Sorry. I dunno what to say.";
+            }
+        }
+    }
+
+    [SerializeField]
+    private List<DialogueLine> lines;
+
+    private Dictionary<int,string> Lines;
+    bool intitialized = false;
+
+    private void Initialize()
+    {
+        Lines = new Dictionary<int, string>();
+        for (int i = 0; i < lines.Count; i++)
+        {
+            Lines.Add(lines[i].ID, lines[i].Text);
+        }
+    }
+
+}
+
+public struct DialogueLine
+{
+    public int ID;
+    public string Text;
+}
